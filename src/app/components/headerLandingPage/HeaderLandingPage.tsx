debc7aa97e3e6a32ef352f44d55aa0c44d75d5db
Erne1917 <ernestofamorim@gmail.com>
2025-09-21 10:41:16 -0300

    feat: ThemeChanger, axio and some layout configuration

---
+++ src/app/components/headerLandingPage/HeaderLandingPage.tsx
@@ -0,0 +1,12 @@
+import styles from "./HeaderLanginPage.module.css";
+
+
+
+export default function HeaderLanginPage() {
+
+    return(
+        <>
+        {/** To implemete yet */}
+        </>
+    )
+}
