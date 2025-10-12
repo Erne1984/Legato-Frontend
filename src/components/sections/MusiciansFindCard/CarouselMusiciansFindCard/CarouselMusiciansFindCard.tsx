964c8645bef52521c24df18b0eeff8511f0b4366
victor-trindade-santos <victortrindade.free@gmail.com>
2025-10-12 19:13:45 -0300

    feat: adjust the arrows in slides and change the progress bar

--- src/components/sections/MusiciansFindCard/CarouselMusiciansFindCard/CarouselMusiciansFindCard.tsx
+++ src/components/sections/MusiciansFindCard/CarouselMusiciansFindCard/CarouselMusiciansFindCard.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import styles from './CarouselMusiciansFindCard.module.css'
 import Icon from '@/components/ui/Icon/Icon'
-import Slider from "react-slick";
 
 type SlideItem = {
     type: "image" | "video";
     src: string;
-    distance?: string;
 };
 
 type CarouselCardProps = {
@@ -17,100 +15,102 @@ type CarouselCardProps = {
     skills: string[];
 };
 
-export default function CarouselMusiciansFindCard({ slides, name, skills }: CarouselCardProps) {
+export default function CarouselMusiciansFindCard({
+    slides,
+    name,
+    skills
+}: CarouselCardProps) {
+    // Active estate slide
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    console.log(slides);
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        beforeChange: (_: number, next: number) => setCurrentSlide(next),
-        arrows: true,
-        nextArrow: <CustomArrow direction="right" />,
-        prevArrow: <CustomArrow direction="left" />,
+    // Function to go for the next slide
+    const nextSlide = () => {
+        setCurrentSlide((prev) => (prev + 1) % slides.length); //loop infinito
     };
 
-    function CustomArrow({
-        className,
-        style,
-        onClick,
-        direction,
-    }: {
-        className?: string;
-        style?: React.CSSProperties;
-        onClick?: () => void;
-        direction: "left" | "right";
-    }) {
-        return (
-            <div
-                className={`${styles.custom_arrow} ${styles[direction]}`}
-                style={style}
-                onClick={onClick}
-            />
-        );
+    // Function to go for the prev slide
+    const prevSlide = () => {
+        setCurrentSlide((prev) => (prev - 1 + + slides.length) % slides.length); //loop infinito
     }
+
     return (
-        <>
-            <div className={styles.image_container_section_card}>
-                {/* Carousel Find Musicians */}
-                <div className={styles.carousel_background}>
-                    <Slider {...settings}>
-                        {slides.map((slide, index) => (
-                            <div key={index} className={styles.slide_item}>
-                                {slide.type === "video" ? (
-                                    <video
-                                        className={styles.background_media}
-                                        src={slide.src}
-                                        autoPlay
-                                        loop
-                                        muted
-                                    />
-                                ) : (
-                                    <div
-                                        className={styles.background_media}
-                                        style={{ backgroundImage: `url(${slide.src})` }}
-                                    />
-                                )}
+        <div className={styles.carousel_container}>
+            {/* Slides */}
+            <div className={styles.slider_wrapper}>
+                {slides.map((slide, index) => (
+                    <div
+                        key={index}
+                        className={`${styles.slide} ${index === currentSlide ? styles.active : ""}`}
+                    >
+                        {slide.type === "video" ? (
+                            <video
+                                className={styles.media}
+                                src={slide.src}
+                                autoPlay
+                                loop
+                                muted
+                            />
+                        ) : (
+                            <div
+                                className={styles.media}
+                                style={{ backgroundImage: `url(${slide.src})` }}
+                            />
+                        )}
+                    </div>
+                ))}
+
+                {/* Manual arrow */}
+                <button className={`${styles.arrow} ${styles.left}`} onClick={prevSlide}>
+                    <Icon name="LeftArrow" className={styles.leftArrowIcon}></Icon>
+                </button>
+                <button className={`${styles.arrow} ${styles.right}`} onClick={nextSlide}>
+                    <Icon name="RightArrow" className={styles.rightArrowIcon}></Icon>
+                </button>
+            </div>
+
+
+            {/* Musician info */}
+            <div className={styles.overlay_content}>
+                <p className={styles.distance_tag}>
+                    <Icon name="mapPin" className={styles.mapPin_icon}></Icon> 3km
+                </p>
+                <div className={styles.container_intro_info}>
+                    <h1 className={styles.musician_name}>
+                        {name}
+                    </h1>
+                    <div className={styles.musician_skills}>
+                        {skills.map((skill, i) => (
+                            <div key={i} className={styles.musician_skills_item}>
+                                {skill}
                             </div>
                         ))}
-                    </Slider>
-                </div>
-
-                {/* Musician info */}
-                <div className={styles.overlay_content}>
-                    <p className={styles.distance_tag}>
-                        <Icon name="mapPin" className={styles.mapPin_icon}></Icon> 3km
-                    </p>
-                    <div className={styles.container_intro_info}>
-                        <h1 className={styles.musician_name}>
-                            {name}
-                        </h1>
-                        <div className={styles.musician_skills}>
-                            {skills.map((skill, i) => (
-                                <div key={i} className={styles.musician_skills_item}>
-                                    {skill}
-                                </div>
-                            ))}
-                        </div>
                     </div>
                 </div>
-
-
-                {/* Progress Slider Bar */}
-                <div className={styles.progress_bar}>
-                    {slides.map((_, index) => (
-                        <div
-                            key={index}
-                            className={`${styles.progress_dot} 
-                        ${index === currentSlide ? styles.active_dot : ""}`}
-                        />
-                    ))}
-                </div>
             </div>
-        </>
+
+
+            {/* Progress Slider Bar */}
+            {/* Barra de progresso estilo stories */}
+            <div className={styles.stories_progress_container}>
+                {slides.map((_, index) => (
+                    <div
+                        key={index}
+                        className={styles.story_bar_wrapper}
+                    >
+                        <div
+                            key={`${index}-${currentSlide}`}
+                            className={`${styles.story_bar_fill} ${index < currentSlide
+                                ? styles.filled
+                                : index === currentSlide
+                                    ? styles.bar_active
+                                    : ""
+                                }`}
+                        />
+                    </div>
+                ))}
+            </div>
+
+        </div>
     )
 }
 
