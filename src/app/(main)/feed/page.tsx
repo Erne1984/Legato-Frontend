bbcb9a5c87c3dee77c9e3e4796d978d1f0ab5557
Erne1917 <ernestofamorim@gmail.com>
2025-10-16 21:16:06 -0300

    feat: feed style

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -1,19 +1,42 @@
 import Sidebar from "@/components/sections/feed/Sidebar/Sidebar";
 import styles from "./FeedPage.module.css";
 import CreatePost from "@/components/sections/feed/CreatePost/CreatePost";
+import SuggestedProfiles from "@/components/ui/SuggestedProfiles/SuggestedProfiles";
+import Post from "@/components/ui/Post/Post";
+import posts from "../../../components/sections/users/UsersContent/data.json";
+import Footer from "@/components/ui/Footer/Footer";
 
 export default function FeedPage() {
+  return (
+    <div className={styles.container_feed_wrapper}>
+      <div className={styles.container_main_content}>
+        <Sidebar />
 
-    return(
-        <div className={styles.container_feed_wrapper}>
+        <div className={styles.center_content}>
+          <CreatePost
+            imgUrl={
+              "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdkHhjzlsAqhkYuCH0Ly2ClT2jM_EZGqfqP179vw1jo5TCFdjZiL5Q3iYiUhj6L3XokuhpdaTY-mJY4ehQ08JR_LB4G_yjZYllBAnkEuX1"
+            }
+            username="ErnePlayson"
+          />
 
-            <Sidebar/>
-
-            <div>
-
-                <CreatePost  imgUrl={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdkHhjzlsAqhkYuCH0Ly2ClT2jM_EZGqfqP179vw1jo5TCFdjZiL5Q3iYiUhj6L3XokuhpdaTY-mJY4ehQ08JR_LB4G_yjZYllBAnkEuX1"} username="ErnePlayson" />
-
-            </div>
+          {posts.map((post, index) => (
+            <Post
+              key={index}
+              author={post.author}
+              timestamp={post.timestamp}
+              location={post.location}
+              content={post.content}
+              likes={post.likes}
+              comments={post.comments}
+            />
+          ))}
         </div>
-    )
+
+        <SuggestedProfiles userImg="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdkHhjzlsAqhkYuCH0Ly2ClT2jM_EZGqfqP179vw1jo5TCFdjZiL5Q3iYiUhj6L3XokuhpdaTY-mJY4ehQ08JR_LB4G_yjZYllBAnkEuX1" />
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
