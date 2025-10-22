7ddaee1dfa733569c6582c0c1d2d2528d15f4fea
Erne1917 <ernestofamorim@gmail.com>
2025-10-22 10:28:01 -0300

    refactor: Adjusting margin in chatPage

--- src/app/(main)/chat/page.tsx
+++ src/app/(main)/chat/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import styles from "./chat.module.css";
 import ChatArea from "@/components/sections/chat/ChatArea/ChatArea";
- 
+
 import { MessageType } from "@/types/MessageType";
 import SidebarChat from "@/components/sections/chat/SidebarChat/SidebarChat";
 import SidebarChatMobile from "@/components/sections/chat/SidebarChat/SidebarChatMobile";
@@ -15,29 +15,35 @@ export default function ChatPage() {
   const messages: MessageType[] = [
     { from: "user", text: "Lorem ipsum dolor sit amet", time: "9:46" },
     { from: "me", text: "Consectetur adipiscing elit", time: "9:48" },
-    { from: "me", text: "Vivamus fringilla lacus nec metus bibendum", time: "9:50" },
+    {
+      from: "me",
+      text: "Vivamus fringilla lacus nec metus bibendum",
+      time: "9:50",
+    },
   ];
 
   return (
     <div className={styles.container_chat_wrapper}>
-      <SidebarChat
-        selectedChat={selectedChat}
-        setSelectedChat={setSelectedChat}
-      />
+ 
+        <SidebarChat
+          selectedChat={selectedChat}
+          setSelectedChat={setSelectedChat}
+        />
 
-      <SidebarChatMobile
-        selectedChat={selectedChat}
-        setSelectedChat={setSelectedChat}
-        isOpen={isSidebarOpen}
-        setIsOpen={setIsSidebarOpen}
-      />
+        <SidebarChatMobile
+          selectedChat={selectedChat}
+          setSelectedChat={setSelectedChat}
+          isOpen={isSidebarOpen}
+          setIsOpen={setIsSidebarOpen}
+        />
 
-      <ChatArea
-        onToggleSidebar={() => setIsSidebarOpen(true)}
-        messages={messages}
-        message={message}
-        setMessage={setMessage}
-      />
+        <ChatArea
+          onToggleSidebar={() => setIsSidebarOpen(true)}
+          messages={messages}
+          message={message}
+          setMessage={setMessage}
+        />
+ 
     </div>
   );
 }
