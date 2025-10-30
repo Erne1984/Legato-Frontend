8949035ae3fb1e6461a6ce602375cb7c73504a64
Erne1917 <ernestofamorim@gmail.com>
2025-10-30 03:05:47 -0300

    feat: fix search page

---
+++ src/app/(main)/search/[query]/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./search.module.css";
+
+
+export default function SearchPage() {
+
+
+    return(
+        <div className={styles.container_search_wrapper}>
+
+        </div>
+    )
+}
